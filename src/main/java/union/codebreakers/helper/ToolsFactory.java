d357599369a068976c31ba8f4a16c80dc7b9c572
Lukas Polak <polakl@union.edu>
2014-02-08 17:34:17 -0500

    Created Tools factory

---
+++ src/main/java/union/codebreakers/helper/ToolsFactory.java
@@ -0,0 +1,34 @@
+package union.codebreakers.helper;
+
+/**
+ * Tools factory handles managing only one instance of every auxiliary tool at the time
+ * @author 3lf
+ */
+public class ToolsFactory {
+    
+    static private Intersection IntersectionTool = null;
+    static private CollisionHandlerGeneric CollisionHandlerTool = null;
+    
+    /*
+     * Gets an instance of intersection tools
+     * @return Instance of Intersection tool
+    */
+    public static Intersection getIntersection(){
+        if( ToolsFactory.IntersectionTool == null ) {
+            ToolsFactory.IntersectionTool = new Intersection();
+        }
+        return ToolsFactory.IntersectionTool;
+    }
+    
+    /*
+     * Gets an instance of collision tools
+     * @return Instance of CollisionHandlerGeneric tool
+    */
+    public static CollisionHandlerGeneric getCollisionHandler(){
+        if( ToolsFactory.CollisionHandlerTool == null ) {
+            ToolsFactory.CollisionHandlerTool = new CollisionHandler();
+        }
+        return ToolsFactory.CollisionHandlerTool;
+    }
+
+}
