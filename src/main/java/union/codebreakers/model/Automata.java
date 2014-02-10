9e67d5ef69c9147873b8b1f74e061455d888672d
Teddy Kitchen <teddykitchen@teddys-mbp-2.union.edu>
2014-02-09 21:48:53 -0500

    compiling now automata interface relating to states

--- src/main/java/union/codebreakers/model/Automata.java
+++ src/main/java/union/codebreakers/model/Automata.java
@@ -30,25 +30,87 @@ public  interface Automata
     * adds a state to the automata
     * @param the state to add
     */
-//    public void addState(State toAdd);
-//    
-//    public Label getStateLabel(State myState);
-//    
-//    public void setStateLabel(State myState, Label toSet);
-//    
-//    public int getStatePathNum(State myState);
-//    
-//    public Iterable<Path> getStatePaths(State myState);
-//    
-//    public Point getStatePos(State myState);
-//    
-//    public void setStatePos(State myState, Point position);
-//    
-//    public void setStatePos(State myState, int x, int y);
-//   
-//    public StateType getStateType(State myState);
-//    
-//    public void setStateType(State myState, StateType myType);
+    public void addState(State toAdd);
+    
+   /**      
+    *   gets the label of a state
+    * 
+    * @return label of the state
+    * @param myState
+    */
+    
+    public Label getStateLabel(State myState);
+    
+    
+    /**      
+    *
+    * sets the label of a state to the indicated label
+    * 
+    * @param myState
+    * @param toSet
+    */
+    public void setStateLabel(State myState, Label toSet);
+    
+    
+    /**      
+    *   gets the number of paths from a state
+    * @return the number of paths from a given state
+    * @param myState
+    */
+    public int getStatePathNum(State myState);
+    
+    
+    /**      
+    * gets an iterable of paths from a state
+    * @return iterable collecton of a state's outgoing paths
+    * @param myState
+    */
+    public Iterable<Path> getStatePaths(State myState);
+    
+    
+    /**      
+    *   gets the position of a state
+    * @return Point for the position of the state
+    * @param  myState
+    */
+    public Point getStatePos(State myState);
+    
+    
+    /**      
+    *
+    * sets the position of a state
+    * 
+    * @param myState
+    * @param position
+    */
+    public void setStatePos(State myState, Point position);
+    
+    /**      
+    *
+    * sets the position of a state
+    * 
+    * @param myState
+    * @param x
+    * @param y
+    */
+    public void setStatePos(State myState, int x, int y);
+   
+    
+    /**      
+    *
+    * gets the type of a given state
+    * @return StateType of this State
+    * @param myState
+    */
+    public StateType getStateType(State myState);
+    
+    
+    /**      
+    *sets the type of a given state
+    * @param myState
+    * @param myType
+    */
+    public void setStateType(State myState, StateType myType);
     
    
     
--- src/main/java/union/codebreakers/model/ModelAutomata.java
+++ src/main/java/union/codebreakers/model/ModelAutomata.java
@@ -1,5 +1,7 @@
 package union.codebreakers.model;
+import java.awt.Point;
 import java.util.ArrayList;
+import union.codebreakers.helper.StateType;
 
 
 
@@ -26,15 +28,125 @@ public class ModelAutomata implements Automata
         
         
         
-        /**
-         * 
-         * adds a state to the automata
-         * @param the state to add
-        */
-        public void addState(State toAdd)
-        {
-            
-        }
+    //METHODS INVOLVING STATES IMPLEMENT THEM
+    
+   /**      
+    *
+    * adds a state to the automata
+    * @param the state to add
+    */
+    public void addState(State toAdd)
+    {
+        
+    }
+    
+   /**      
+    *   gets the label of a state
+    * 
+    * @return label of the state
+    * @param myState
+    */
+    
+    public Label getStateLabel(State myState)
+    {
+        return null;
+    }
+    
+    
+    /**      
+    *
+    * sets the label of a state to the indicated label
+    * 
+    * @param myState
+    * @param toSet
+    */
+    public void setStateLabel(State myState, Label toSet)
+    {
+        
+    }
+    
+    
+    /**      
+    *   gets the number of paths from a state
+    * @return the number of paths from a given state
+    * @param myState
+    */
+    public int getStatePathNum(State myState)
+    {
+        return 0;
+    }
+    
+    
+    /**      
+    * gets an iterable of paths from a state
+    * @return iterable collecton of a state's outgoing paths
+    * @param myState
+    */
+    public Iterable<Path> getStatePaths(State myState)
+    {
+        return null;
+    }
+    
+    
+    /**      
+    *   gets the position of a state
+    * @return Point for the position of the state
+    * @param  myState
+    */
+    public Point getStatePos(State myState)
+    {
+        return null;
+    }
+                 
+    
+    
+    /**      
+    *
+    * sets the position of a state
+    * 
+    * @param myState
+    * @param position
+    */
+    public void setStatePos(State myState, Point position)
+    {
+        
+    }
+    
+    /**      
+    *
+    * sets the position of a state
+    * 
+    * @param myState
+    * @param x
+    * @param y
+    */
+    public void setStatePos(State myState, int x, int y)
+    {
+        
+    }
+   
+    
+    /**      
+    *
+    * gets the type of a given state
+    * @return StateType of this State
+    * @param myState
+    */
+    public StateType getStateType(State myState)
+    {
+        return null;
+    }
+    
+    
+    /**      
+    *sets the type of a given state
+    * @param myState
+    * @param myType
+    */
+    public void setStateType(State myState, StateType myType)
+    {
+        
+    }
         
         
         
