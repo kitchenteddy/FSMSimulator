39bece5c752174bc24e3cf36c67b790da65f91f0
Teddy Kitchen <teddykitchen@teddys-mbp-2.union.edu>
2014-02-19 11:44:58 -0500

    with empty test suite

---
+++ src/test/java/union/codebreakers/tests/TestSuite.java
@@ -0,0 +1,18 @@
+package union.codebreakers.tests;
+
+
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+    
+	FSATest.class
+
+})
+public class TestSuite
+{ // no implementation needed; above annotations do the work.
+}
