1fcdd9309b42f6db455d06c75a1045843b90556c
Teddy Kitchen <teddykitchen@teddys-mbp-2.home>
2014-02-20 03:04:06 -0500

    working on path tests

--- src/test/java/union/codebreakers/tests/PathTests.java
+++ src/test/java/union/codebreakers/tests/PathTests.java
@@ -1,5 +1,7 @@
 package union.codebreakers.tests;
 
+import java.awt.Point;
+import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
@@ -11,24 +13,66 @@ import org.junit.Before;
 import org.junit.After;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import union.codebreakers.helper.enums.StateType;
+import union.codebreakers.model.Automaton;
+import union.codebreakers.model.Label;
+import union.codebreakers.model.ModelAutomaton;
+import union.codebreakers.model.ModelLabel;
+import union.codebreakers.model.ModelState;
+import union.codebreakers.model.State;
 
 
 
 @RunWith(JUnit4.class)
 public class PathTests
 {
+    private Automaton myAutomaton;
     
+    private State s1;
+    private State s2;
+    
+    private Point s1Pos;    
+    private Label s1L;
+    
+    private Point s2Pos;  
+    private Label s2L;
+    
+    ArrayList myList;
     
     @Before
     public void setUp()
     {
+        myAutomaton = new ModelAutomaton();
         
+        s1Pos = new Point(0,0);
+        s1L = new ModelLabel();
+        s1L.setName("State1");
+        
+        s2Pos = new Point(0,0);
+        s2L = new ModelLabel();
+        s2L.setName("State2");
+        
+        s1 = new ModelState(StateType.eNormal, s1Pos, s1L, myAutomaton);
+        s2 = new ModelState(StateType.eNormal, s2Pos, s2L, myAutomaton);
+        
+        myList = new ArrayList<State>();
     }
 
     @After
     public void tearDown()
     {
+        s1Pos = null;
+        s1L = null;
         
+        
+        s2Pos = null;
+        s2L = null;
+        
+        
+        s1 = null;
+        s2 = null;
+        
+        myList = null;
     }
     
     @Test
@@ -37,6 +81,12 @@ public class PathTests
 	   
        assertEquals(true,true);
 
+    }
+    
+    @Test
+    public void getPoints()
+    {
+        
     }
 
 
