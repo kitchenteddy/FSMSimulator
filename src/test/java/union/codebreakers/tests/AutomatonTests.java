b5125f3131064ff1c4731aad929270d2c6a90e53
Joshua Loew <joshualoew@joshuas-mbp.union.edu>
2014-02-20 08:16:34 -0500

    fixed minor formatting issues

--- src/test/java/union/codebreakers/tests/AutomatonTests.java
+++ src/test/java/union/codebreakers/tests/AutomatonTests.java
@@ -1,19 +1,14 @@
 package union.codebreakers.tests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.Before;
 import org.junit.After;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import union.codebreakers.model.*;
 import java.awt.Point;
-import union.codebreakers.helper.*;
 import union.codebreakers.helper.enums.StateType;
 import java.util.ArrayList;
 
